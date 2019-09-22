9b3bf9ecf9ca284649fa3b7156454a8e4b7d5d78
Tom Wang <wxy123@yeah.net>
2019-09-22 16:48:03 +0800

    Create, edit, delete item by form

--- app.py
+++ app.py
@@ -1,4 +1,4 @@
-from flask import url_for
+from flask import url_for, request, redirect, flash
 from flask import Flask, render_template
 from flask_sqlalchemy import SQLAlchemy
 import os
@@ -15,6 +15,7 @@ app = Flask(__name__)
 
 app.config['SQLALCHEMY_DATABASE_URI'] = prefix + os.path.join(app.root_path, 'data.db')
 app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False #关闭对模型修改的监控
+app.config['SECRET_KEY'] = 'dev'
 db = SQLAlchemy(app)
 
 @app.cli.command()
@@ -26,11 +27,50 @@ def initdb(drop):
     db.create_all()
     click.echo('Initialized database.')
 
-@app.route('/')
+@app.route('/', methods=['GET', 'POST'])
 def index():
+    if request.method == 'POST':
+        title = request.form.get('title')
+        year = request.form.get('year')
+        if not title or not year or len(year) != 4 or len(title) > 60:
+            flash('输入格式错误')
+            return redirect(url_for('index'))
+        movie = Movie(title=title, year=year)
+        db.session.add(movie)
+        db.session.commit()
+        flash('添加成功')
+        return redirect(url_for('index'))
+
     user = User.query.first()
     movies = Movie.query.all()
     return render_template('index.html', movies=movies)
+
+@app.route('/movie/edit/<int:movie_id>', methods=['GET', 'POST'])
+def edit(movie_id):
+    movie = Movie.query.get_or_404(movie_id)
+    if request.method == 'POST':
+        title = request.form['title']
+        year = request.form['year']
+
+        if not title or not year or len(year) != 4 or len(title) > 60:
+            flash("输入格式错误")
+            return redirect(url_for('edit', movie_id=movie_id))
+
+        movie.title = title
+        movie.year = year
+        db.session.commit()
+        flash('编辑成功')
+        return redirect(url_for('index'))
+
+    return render_template('edit.html', movie=movie)
+
+@app.route('/movie/delete/<int:movie_id>', methods=['POST'])
+def delete(movie_id):
+    movie = Movie.query.get_or_404(movie_id)
+    db.session.delete(movie)
+    db.session.commit()
+    flash('已删除')
+    return redirect(url_for('index'))
 
 class User(db.Model):
     id = db.Column(db.Integer, primary_key=True)
